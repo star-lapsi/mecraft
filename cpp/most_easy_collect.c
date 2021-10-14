02a7fc034cc10e840e8c0f0a8bbcdac634eca47b
star-lapsi <hypno_disc3@126.com>
2021-10-14 20:10:40 +0800

    daily updating 2021.10.14

---
+++ cpp/most_easy_collect.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+void main()
+{
+    int a;
+    scanf("%d",&a);
+    prinff("your input: %d\n",a);
+}
---
+++ cpp/most_easy_handle.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main()
+{
+    int i;
+    scanf("%d",&i);
+    if(i==0) printf("i==0\n");
+    else printf("i<>0\n");
+}
